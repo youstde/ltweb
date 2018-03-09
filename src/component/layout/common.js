23d6a8f5df660123ddcca8b887892ef413a61f83
ns <ns@hongdeMacBook-Pro.local>
2018-03-09 18:44:40 +0800

    fix

--- src/component/layout/common.js
+++ src/component/layout/common.js
@@ -1,5 +1,4 @@
 /*
 这里写一些公共的js逻辑代码
  */
-(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=&quot;qWFURt9XtF3ZAGCAnVVTGbHuIrGkx9W6&quot;&services=&t=20180201111639"></script>');})();
 console.log('common');
