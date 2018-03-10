4796c1154c9a03572218d96d7a36f3f24c9ba381
ns <ns@hongdeMacBook-Pro.local>
2018-03-10 15:08:06 +0800

    register

--- src/component/layout/common.js
+++ src/component/layout/common.js
@@ -2,13 +2,23 @@
 这里写一些公共的js逻辑代码
  */
 $(document).ready(function(){
+    //head部分
     var oLi = $(".centerLi")[0];
-    console.log(oLi)
     $(".centerLi").click(function () {
         var _this = $(this);
         setTimeout(function () {
             _this.css('borderBottom','4px solid #e83928');
         },500);
+    })
+/**
+ * 注册弹窗
+ *
+ */
+    $('.register').click(function () {
+        $('#register').css("display","block");
+        $('#closeReg').click(function () {
+            $('#register').css('display','none');
+        })
     })
 });
 console.log('common');
