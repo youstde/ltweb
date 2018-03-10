d5c0aca0195951093204ed307c83234b4085fd16
ns <ns@hongdeMacBook-Pro.local>
2018-03-10 16:58:54 +0800

    微调one

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -18,38 +18,47 @@ window.onload=function () {
     }
 
     //数字滚动
-    var mathTimer = null,num1 = 0,num2 = 1,num3 = 0,num4 = 0,timer1 = null,timer2=null,timer3=null,timer4=null;
-    var oData1 = document.querySelector('#data1');
-    var oData2 = document.querySelector('#data2');
-    var oData3 = document.querySelector('#data3');
-    var oData4 = document.querySelector('#data4');
+    console.log($('.btData').scrollTop());
+    console.log($(document).scrollTop())
+    var isCount = true;
+    $(document).scroll(function(){
+    var numScrollTop = $(document).scrollTop();
+    if(isCount && numScrollTop >= 1802){
+        var mathTimer = null,num1 = 0,num2 = 1,num3 = 0,num4 = 0,timer1 = null,timer2=null,timer3=null,timer4=null;
+        var oData1 = document.querySelector('#data1');
+        var oData2 = document.querySelector('#data2');
+        var oData3 = document.querySelector('#data3');
+        var oData4 = document.querySelector('#data4');
+        isCount = false;
+        timer1 = setInterval(function () {
+            num1 += 2;
+            if(num1 >= 200){
+                clearInterval(timer1);
+            }
+            oData1.innerHTML = num1;
+        },15);
+        timer2 = setInterval(function () {
+            num2 ++;
+            if(num2 >= 3){
+                clearInterval(timer2);
+            }
+            oData2.innerHTML = num2 + "亿";
+        },600);
+        timer3 = setInterval(function () {
+            num3 ++;
+            if(num3 >= 15){
+                clearInterval(timer3);
+            }
+            oData3.innerHTML = num3 + "亿";
+        },100);
+        timer4 = setInterval(function () {
+            num4 += 1000;
+            if(num4 >= 15000){
+                clearInterval(timer4);
+            }
+            oData4.innerHTML = num4;
+        },100);
+    }
 
-    timer1 = setInterval(function () {
-        num1 += 2;
-        if(num1 >= 200){
-            clearInterval(timer1);
-        }
-        oData1.innerHTML = num1;
-    },15);
-    timer2 = setInterval(function () {
-        num2 ++;
-        if(num2 >= 3){
-            clearInterval(timer2);
-        }
-        oData2.innerHTML = num2 + "亿";
-    },600);
-    timer3 = setInterval(function () {
-        num3 ++;
-        if(num3 >= 15){
-            clearInterval(timer3);
-        }
-        oData3.innerHTML = num3 + "亿";
-    },100);
-    timer4 = setInterval(function () {
-        num4 += 1000;
-        if(num4 >= 15000){
-            clearInterval(timer4);
-        }
-        oData4.innerHTML = num4;
-    },100);
+    })
 }
--- src/page/laituiInt/index.js
+++ src/page/laituiInt/index.js
@@ -5,18 +5,60 @@ var $Dom = function (dom) {
     return document.querySelectorAll(dom);
 }
 
-console.log($Dom('.listTopUl .topUlList'));
 var familyLi = $Dom('.listTopUl .topUlList');
 var oPhone = $Dom('.phone')[0];
 var oContent = $Dom('.phoneContent');
+var isLeft = false;
+/*
+ *
+ * topUlList 透明度变化
+ */
+$('.topUlList').click(function () {
+    console.log($(this).siblings());
+    $(this).children('img').css("opacity","1");
+    $(this).siblings().children('img').css("opacity","0.6");
+
+})
+
+
+    /*
+     *
+     * phone图片
+     */
+$('.btnLeft').click(function () {
+    if(!isLeft){
+        $(this).css("backgroundColor","#1599ff");
+        $('.btnRight').css("backgroundColor","#f3f8fe");
+        $('.contentImg').animate({left:'0'});
+    }else{
+        isLeft = true;
+    }
+
+})
+$('.btnRight').click(function () {
+    $(this).css("backgroundColor","#1599ff");
+    $('.btnLeft').css("backgroundColor","#f3f8fe");
+    $('.contentImg').animate({left:'-259px'});
+})
 for(let i = 0; i <= familyLi.length - 1 ;i ++){
     oContent[i].style.display = "none";
     oContent[0].style.display = "block";
     familyLi[i].onclick = function (e) {
         var e = event || window.event;
+        event.stopPropagation();
         for(var index = 0;index <= 4; index ++){
             oContent[index].style.display = "none";
         }
         oContent[i].style.display = 'block';
+        if(i == 0 || i == 3){
+            $Dom('.phoneBtn')[0].style.display = "block";
+            // $('.btnLeft').click(function () {
+            //     alert(1);
+            // })
+            // console.log($('.btnLeft'));
+        }else{
+            $Dom('.phoneBtn')[0].style.display = "none";
+            return false;
+        }
     }
 }
